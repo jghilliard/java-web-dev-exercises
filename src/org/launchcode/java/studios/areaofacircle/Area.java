900b541a6f65d512ac2b574bff9b1eb965f9b87a
Josh Hilliard <125423113+jghilliard@users.noreply.github.com>
2023-03-27 19:03:11 -0500

    Studio complete, starting bonus mission.

---
+++ src/org/launchcode/java/studios/areaofacircle/Area.java
@@ -0,0 +1,19 @@
+package org.launchcode.java.studios.areaofacircle;
+
+import java.util.Scanner;
+//import java.lang.Math.*;
+public class Area {
+    public static void main(String[] args) {
+        double radius;
+        double area;
+        Scanner input;
+
+        input = new Scanner(System.in);
+        System.out.println("What is the radius of your circle?");
+        radius = input.nextDouble();
+        input.close();
+
+        area = Circle.getArea(radius);
+        System.out.println("The area of your circle is " + area +"!");
+    }
+}
