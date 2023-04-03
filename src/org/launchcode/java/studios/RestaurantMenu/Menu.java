1d98ee41293166278d8081fc9fe50123d1798333
Josh Hilliard <125423113+jghilliard@users.noreply.github.com>
2023-04-03 18:56:05 -0500

    Classes + Objects Studio

---
+++ src/org/launchcode/java/studios/RestaurantMenu/Menu.java
@@ -0,0 +1,19 @@
+package org.launchcode.java.studios.RestaurantMenu;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Menu {
+    public static void main(String[] args) {
+        ArrayList fullMenu;
+        String lastUpdated;
+//        Scanner input;
+//        String phrase;
+//
+//        input = new Scanner(System.in);
+//        System.out.println("Please enter a phrase.");
+//        phrase = input.nextLine();
+//        input.close();
+    }
+
+}
