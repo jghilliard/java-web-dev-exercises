7b0a2e0d15f86e12a6cd89a709c4d933f76bb2c3
Josh Hilliard <125423113+jghilliard@users.noreply.github.com>
2023-04-03 19:54:40 -0500

    Another Update

--- src/org/launchcode/java/studios/RestaurantMenu/Menu.java
+++ src/org/launchcode/java/studios/RestaurantMenu/Menu.java
@@ -1,19 +1,53 @@
 package org.launchcode.java.studios.RestaurantMenu;
 
+import java.awt.*;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Menu {
     public static void main(String[] args) {
+        //String name;
         ArrayList fullMenu;
         String lastUpdated;
-//        Scanner input;
-//        String phrase;
-//
-//        input = new Scanner(System.in);
-//        System.out.println("Please enter a phrase.");
-//        phrase = input.nextLine();
-//        input.close();
+        restart();
+    }
+
+    public static void restart() {
+        Scanner input;
+        String phrase;
+        Integer choice;
+
+        input = new Scanner(System.in);
+        System.out.println("Please select an option\n" +
+                "0: Edit Menu Date\n" +
+                "1: View Menu Items\n" +
+                "2: Add Menu Item");
+        choice = input.nextInt();
+        switch (choice) {
+            case 0:
+                editMenu();
+                break;
+            case 1:
+                viewMenu();
+                break;
+            case 2:
+                addItem();
+                break;
+            default:
+                restart();
+        }
+        input.close();
+    }
+
+    public static void editMenu() {
+
+    }
+
+    public static void viewMenu() {
+    }
+
+    public static void addItem() {
     }
 
 }
+
